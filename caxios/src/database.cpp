adb0c449fac4c4c57ef0e796772a7081067d77bc
v_yuanwenbin <v_yuanwenbin@MacintoshdeMacBook-Pro.local>
2020-09-25 12:59:38 +0800

    change database action of put orperator

--- caxios/src/database.cpp
+++ caxios/src/database.cpp
@@ -95,11 +95,26 @@ namespace caxios {
     MDB_val key, datum;
     key.mv_data = reinterpret_cast<void*>(&k);
     key.mv_size = sizeof(size_t);
-    datum.mv_data = pData;
-    datum.mv_size = len;
+    if (falg == MDB_CURRENT) { // update value of key
+      rc = mdb_cursor_get(cursor, &key, &datum, MDB_FIRST);
+      datum.mv_data = pData;
+      datum.mv_size = len;
+      int cursor_flag = 0;
+      if (rc != MDB_NOTFOUND) {
+        cursor_flag = MDB_CURRENT;
+      }
+      if (int rc = mdb_cursor_put(cursor, &key, &datum, cursor_flag)) {
+        std::cout << "mdb_put fail: " << err2str(rc) << std::endl;
+        m_dOperator = NORMAL;
+        mdb_cursor_close(cursor);
+        return false;
+      }
+    }
+    // append value to key
     if (int rc = mdb_cursor_put(cursor, &key, &datum, 0)) {
       std::cout << "mdb_put fail: " << err2str(rc) << std::endl;
       m_dOperator = NORMAL;
+      mdb_cursor_close(cursor);
       return false;
     }
     mdb_cursor_close(cursor);
@@ -124,6 +139,7 @@ namespace caxios {
     //}
     if (rc = mdb_cursor_get(cursor, &key, &datum, MDB_LAST)) {
       std::cout << "mdb_get fail: " << err2str(rc) << std::endl;
+      mdb_cursor_close(cursor);
       return false;
     }
     len = datum.mv_size;
