20802267005b7ca1caa24b4b90751639f7d36874
v_yuanwenbin <v_yuanwenbin@baidu.com>
2020-11-16 20:25:51 +0800

    update guider

--- src/public/CivetConfig.js
+++ src/public/CivetConfig.js
@@ -20,14 +20,7 @@ export class CivetConfig {
           db: {
             path: userDir + '/civet'
           },
-          meta: [
-            {name: 'color', value: '主色', type: 'val/array', query: true, size: 3, display: true},
-            {name: 'path', value: '路径', type: 'str', display: true},
-            {name: 'filename', value: '文件名', type: 'str', display: true},
-            {name: 'type', value: '类型', type: 'str', display: true},
-            {name: 'width', value: '宽', type: 'str', display: true},
-            {name: 'height', value: '高', type: 'str', display: true}
-          ]
+          meta: this.schema()
         }
       ]
     }
@@ -77,6 +70,21 @@ export class CivetConfig {
     return resources
   }
 
+  addResource(name, path) {
+    for (let resource of this.config.resources) {
+      if (resource.name === name) {
+        resource.db.path = path
+        return
+      }
+    }
+    this.config.default = name
+    this.config.resources.push({
+      name: name,
+      db: {path: path + '/' + name},
+      meta: this.schema()
+    })
+  }
+
   isMetaDisplay(name, meta) {
     for (let item of meta) {
       if (item.name === name && item.display === true) return true
@@ -87,5 +95,16 @@ export class CivetConfig {
   save() {
     this.config.app.first = false
     fs.writeFileSync(this.configPath, JSON.stringify(this.config))
+  }
+
+  schema() {
+    return [
+      {name: 'color', value: '主色', type: 'val/array', query: true, size: 3, display: true},
+      {name: 'path', value: '路径', type: 'str', display: true},
+      {name: 'filename', value: '文件名', type: 'str', display: true},
+      {name: 'type', value: '类型', type: 'str', display: true},
+      {name: 'width', value: '宽', type: 'str', display: true},
+      {name: 'height', value: '高', type: 'str', display: true}
+    ]
   }
 }
--- src/worker/components/Guider.vue
+++ src/worker/components/Guider.vue
@@ -1,8 +1,50 @@
 <template>
-  <div>Guider</div>
+  <div>
+    <label>配置你的第一个资源库</label>
+    <el-input>{{resourceName}}</el-input>
+    <div style="margin-top: 15px;">
+      <el-input placeholder="请输入内容" v-model="resourceDBPath" class="input-with-select" :disabled="true">
+        <template slot="prepend">资源库路径：</template>
+        <el-button slot="append" icon="el-icon-more" @click="onSelectDBPath()"></el-button>
+      </el-input>
+    </div>
+  <el-button @click="onStartCivet()">启动</el-button>
+  </div>
 </template>
 <script>
+import { remote } from 'electron'
+import { CivetConfig } from '../public/CivetConfig'
+
 export default {
-  name: 'Guider'
+  name: 'Guider',
+  data() {
+    return {
+      resourceName: '',
+      resourceDBPath: ''
+    }
+  },
+  mounted() {},
+  methods: {
+    onStartCivet() {
+      if (this.resourceName.trim().length === 0 || this.resourceDBPath.length === 0) return
+      const config = new CivetConfig()
+      config.addResource(this.resourceName, this.resourceDBPath)
+      config.save()
+      this.$ipcRenderer.send('ready')
+    },
+    onSelectDBPath() {
+      let self = this
+      remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
+        properties: ['openDirectory', 'openFile']
+      }).then(async (data) => {
+        if (data === undefined) return
+        if (data.canceled === true) return
+        self.resourceDBPath = data.filePaths[0]
+        if (self.resourceDBPath) {
+          self.enableTransfer = true
+        }
+      })
+    }
+  }
 }
 </script>
