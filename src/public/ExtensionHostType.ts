d42fb440a11d1e81c01624c473b606a6e8c6a940
webbery <webbery@foxmail.com>
2021-06-01 00:56:52 +0800

    :recycle:代码调整，参考vscode实现劫持内置扩展的require原型

---
+++ src/public/ExtensionHostType.ts
@@ -0,0 +1,14 @@
+
+export enum PropertyType {
+  String = 1,
+  Number = 2,
+  Color = 3,
+  Binary = 4
+}
+
+export enum ViewType {
+  Property = 1,
+  Navigation = 2,
+  Overview = 3,
+  DetailView = 4
+}
