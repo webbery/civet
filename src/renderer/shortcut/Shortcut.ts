f457919b33cc1ed6239e770acdb8b2a5fe23efee
webbery <webbery@foxmail.com>
2022-07-17 19:52:09 +0800

    :bug:fix: 修改快捷键后没有立即生效的问题

--- src/renderer/shortcut/Shortcut.ts
+++ src/renderer/shortcut/Shortcut.ts
@@ -2,7 +2,50 @@
 import { config } from '@/../public/CivetConfig'
 import { localKey } from '@/../public/Utility'
 
+class Accelerator{
+  private _shortcut: Map<string, [(...args: Array<any>) => boolean, any]> = new Map();
+  register(shortcut: string, when: string, func: (...args: Array<any>) => boolean ) {
+    const lower = shortcut.toLowerCase()
+    if (this._shortcut.has(lower)) {
+      console.warn(`shortcut ${shortcut} will be replace`)
+    }
+    this._shortcut.set(lower, [func, when])
+  }
+
+  unregister(shortcut: string) {
+    const lower = shortcut.toLowerCase()
+    if (this._shortcut.has(lower)) {
+      this._shortcut.delete(lower)
+    }
+  }
+
+  updateKey(old: string, current: string) {
+    const lower = old.toLowerCase()
+    const lowerCurrent = current.toLowerCase()
+    if (this._shortcut.has(lower)) {
+      const record = this._shortcut.get(lower)!
+      if (this._shortcut.has(lowerCurrent)) {
+        console.warn(`shortcut ${lowerCurrent} is confict`)
+        const history = this._shortcut.get(lowerCurrent)
+        this._shortcut.set('old.' + lowerCurrent, history!)
+      }
+      this._shortcut.set(lowerCurrent, record)
+      this._shortcut.delete(lower)
+      // console.debug('update key:', lower, 'with', lowerCurrent, this._shortcut)
+    }
+  }
+
+  has(shortcut: string) {
+    return this._shortcut.has(shortcut)
+  }
+
+  get(shortcut: string) {
+    return this._shortcut.get(shortcut)
+  }
+}
+
 let pressedKey: string[] = []
+let shortcut: Accelerator = new Accelerator()
 
 function hasKey(key: string) {
   for (const item of pressedKey) {
@@ -30,56 +73,17 @@ const keyDownHandler = (() => {
     pressedKey.push(key)
     console.info('keyDownHandler:', pressedKey)
     let accelerator = pressedKey.join('+')
-    if (Shortcut.has(accelerator)) {
-      const [func, when] = Shortcut.get(accelerator)!
+    if (shortcut.has(accelerator)) {
+      const [func, when] = shortcut.get(accelerator)!
       return func(when)
     }
+    console.debug('short key not exist:', accelerator)
     return false
   }
 })()
 
 document.addEventListener('keydown', keyDownHandler)
 document.addEventListener('keyup', keyUpHandler)
-
-export class Shortcut{
-  private static _shortcut: Map<string, [(...args: Array<any>) => boolean, any]> = new Map();
-  static register(shortcut: string, when: string, func: (...args: Array<any>) => boolean ) {
-    const lower = shortcut.toLowerCase()
-    if (this._shortcut.has(lower)) {
-      console.warn(`shortcut ${shortcut} will be replace`)
-    }
-    this._shortcut.set(lower, [func, when])
-  }
-
-  static unregister(shortcut: string) {
-    const lower = shortcut.toLowerCase()
-    if (this._shortcut.has(lower)) {
-      this._shortcut.delete(lower)
-    }
-  }
-
-  static updateKey(old: string, current: string) {
-    const lower = old.toLowerCase()
-    if (this._shortcut.has(lower)) {
-      const record = this._shortcut[lower]
-      if (this._shortcut.has(current)) {
-        console.warn(`shortcut ${current} is confict`)
-        const history = this._shortcut[current]
-        this._shortcut['old.' + current] = history
-      }
-      this._shortcut[current] = record
-      delete this._shortcut[lower]
-    }
-  }
-
-  static has(shortcut: string) {
-    return this._shortcut.has(shortcut)
-  }
-
-  static get(shortcut: string) {
-    return this._shortcut.get(shortcut)
-  }
-}
 
 function keyUpHandler(event: any) {
   const key = localKey(event.key).toLowerCase()
@@ -88,3 +92,5 @@ function keyUpHandler(event: any) {
   console.info('keyUpHandler:', event)
   return false
 }
+
+export const Shortcut = shortcut
